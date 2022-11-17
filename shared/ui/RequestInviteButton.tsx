04c3fd2857eeaedcb88f3c16934025c4745e3c98
marouane <marsou431@gmail.com>
2022-11-17 09:41:42 +0100

    feat: create caal to action button component

---
+++ shared/ui/RequestInviteButton.tsx
@@ -0,0 +1,7 @@
+function RequestInviteButton() {
+  return (
+    <button className="text-white bg-gradient-to-r from-lime-green to-bright-cyan py-3 px-8 rounded-full">Request Invite</button>
+  )
+}
+
+export default RequestInviteButton;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./pages/*.{html,js,jsx,ts,tsx}",
+    "./shared/ui/*.{html,js,jsx,ts,tsx}",
+    "./components/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': 'hsl(233, 26%, 24%)',
+        'lime-green': 'hsl(136, 65%, 51%)',
+        'bright-cyan': 'hsl(192, 70%, 51%)',
+      }
+    },
   },
   plugins: [],
 }
