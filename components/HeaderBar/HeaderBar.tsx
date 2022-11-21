55af434f0861304bc77ce39232384634eef7c6fc
marouane <marsou431@gmail.com>
2022-11-21 11:55:47 +0100

    style: adjust header bar padding

--- components/HeaderBar/HeaderBar.tsx
+++ components/HeaderBar/HeaderBar.tsx
@@ -4,16 +4,16 @@ import listItems from "./listItems";
 
 function HeaderBar() {
   return (
-    <header className="bg-white absolute flex justify-between items-center w-full p-4 lg:p-6 lg:px-28 lg:py-0 z-10">
+    <header className="bg-white absolute flex justify-between items-center w-full p-4 md:py-0 lg:px-28 lg:py-0 z-10">
       <Logo />
 
-      <button className="lg:hidden">
+      <button className="md:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </button>
 
-      <nav className="hidden lg:block">
+      <nav className="hidden md:block">
         <ul className="h-16">
           {listItems.map((listItem) => (
             <li key={listItem} className="bg-gradient-to-r from-lime-green to-bright-cyan text-grayish-blue hover:text-dark-blue text-sm inline-block h-full">
@@ -23,7 +23,7 @@ function HeaderBar() {
         </ul> 
       </nav>
 
-      <div className="hidden lg:block">
+      <div className="hidden md:block">
         <RequestInviteButton />
       </div>
     </header> 
