ca51e722b4d4da65a3b0a249c361e72a4de95f5e
marouane <marsou431@gmail.com>
2022-11-22 12:50:04 +0100

    test: Headerbar

--- components/HeaderBar/HeaderBar.test.tsx
+++ components/HeaderBar/HeaderBar.test.tsx
@@ -1,12 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import {
+  fireEvent,
+  getAllByRole,
+  getByRole,
+  render,
+  screen,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
 import HeaderBar from "./HeaderBar";
 import links from "./listItems";
 
 describe("HeaderBar", () => {
   beforeEach(() => {
     render(<HeaderBar />);
-  })
+  });
 
   it("renders", () => {
     const headerBar = screen.getByRole("banner");
@@ -14,7 +20,7 @@ describe("HeaderBar", () => {
   });
 
   it("renders logo", () => {
-    const logo = screen.getByTestId('logo');
+    const logo = screen.getByTestId("logo");
     expect(logo).toBeInTheDocument();
   });
 
@@ -46,5 +52,113 @@ describe("HeaderBar", () => {
   it("renders call to action button", () => {
     const callToActionButton = screen.getByText("Request Invite");
     expect(callToActionButton).toBeInTheDocument();
-  })
-})
+  });
+});
+
+describe("Dialog", () => {
+  beforeEach(() => {
+    render(<HeaderBar />);
+
+    const dialogModalControl = screen.getByTestId("menu");
+    fireEvent.click(dialogModalControl);
+  });
+
+  it("renders on menu click", () => {
+    const dialog = screen.getByRole("dialog");
+    expect(dialog).toBeInTheDocument();
+  });
+
+  it("unrenders on menu click", () => {
+    const dialog = screen.getByRole("dialog");
+    const dialogModalControl = screen.getByTestId("menu");
+
+    fireEvent.click(dialogModalControl);
+    expect(dialog).not.toBeInTheDocument();
+  });
+
+  it("renders navigation", () => {
+    const dialog = screen.getByRole("dialog");
+    const nav = getByRole(dialog, "navigation");
+
+    expect(nav).toBeInTheDocument();
+  });
+
+  it("renders list", () => {
+    const dialog = screen.getByRole("dialog");
+    const list = getByRole(dialog, "list");
+
+    expect(list).toBeInTheDocument();
+  });
+
+  it("renders list items", () => {
+    const dialog = screen.getByRole("dialog");
+
+    const listItems = getAllByRole(dialog, "listitem");
+    expect(listItems.length).toBe(5);
+
+    const listItemLinks = getAllByRole(dialog, "link");
+    expect(listItemLinks.length).toBe(5);
+
+    listItems.forEach((listItem, index) => {
+      expect(listItem).toBeInTheDocument();
+      expect(listItem).toContainElement(listItemLinks[index]);
+      expect(listItemLinks[index]).toHaveTextContent(links[index]);
+    });
+  });
+});
+
+describe("Accessibility", () => {
+  beforeEach(() => {
+    render(<HeaderBar />);
+  });
+
+  it("renders and unrenders on pressing Enter key on menu", () => {
+    const dialogModalControl = screen.getByTestId("menu");
+    fireEvent.keyDown(dialogModalControl, { code: "Enter" });
+
+    const dialog = screen.getByRole("dialog");
+    expect(dialog).toBeInTheDocument();
+
+    fireEvent.keyDown(dialogModalControl, { code: "Enter" });
+    expect(dialog).not.toBeInTheDocument();
+  });
+
+  it("unrenders on pressing Escape key", async () => {
+    const dialogModalControl = screen.getByTestId("menu");
+    fireEvent.keyDown(dialogModalControl, { code: "Enter" });
+
+    const dialog = screen.getByRole("dialog");
+
+    fireEvent.keyDown(dialogModalControl, { code: "Escape" });
+    expect(dialog).not.toBeInTheDocument();
+  });
+
+  it("closes on pressing Escape key on list items", () => {
+    const dialogModalControl = screen.getByTestId("menu");
+    fireEvent.keyDown(dialogModalControl, { code: "Enter" });
+
+    const dialog = screen.getByRole("dialog");
+    const listItemsLinks = getAllByRole(dialog, "link");
+
+    fireEvent.focus(listItemsLinks[2]);
+    fireEvent.keyDown(listItemsLinks[2], { code: "Escape" });
+
+    expect(dialog).not.toBeInTheDocument();
+    expect(dialogModalControl).toHaveFocus();
+  });
+
+  it("traps focus within list items links", () => {
+    const dialogModalControl = screen.getByTestId("menu");
+    fireEvent.keyDown(dialogModalControl, { code: "Enter" });
+
+    const dialog = screen.getByRole("dialog");
+    const listItemsLinks = getAllByRole(dialog, "link");
+    fireEvent.focus(listItemsLinks[4]);
+
+    fireEvent.keyDown(listItemsLinks[4], { code: "Tab" });
+    expect(listItemsLinks[0]).toHaveFocus();
+
+    fireEvent.keyDown(listItemsLinks[0], { code: "Tab", shiftKey: true });
+    expect(listItemsLinks[4]).toHaveFocus();
+  });
+});
