03f3f7329a8f0f5351fc533c156cbb4ae751f675
marouane <marsou431@gmail.com>
2022-11-22 12:25:38 +0100

    refactor: format with prettier

--- components/HeaderBar/HeaderBar.tsx
+++ components/HeaderBar/HeaderBar.tsx
@@ -15,44 +15,44 @@ function HeaderBar() {
 
     document.body.style.overflowY = "hidden";
     document.body.style.height = "100vh";
-  }
+  };
 
   const closeDialog = () => {
     setShowDialog(false);
 
     document.body.style.overflowY = "initial";
     document.body.style.height = "initial";
-  }
+  };
 
   const focusOnDialogControl = () => {
     const dialogControl = dialogControlRef.current!;
     dialogControl.focus();
-  }
+  };
 
   const focusOnFirstListItem = () => {
     const firstListItem = firstListItemRef.current!;
     firstListItem.focus();
-  }
+  };
 
   const focusOnLastListItem = () => {
     const lastListItem = lastListItemRef.current!;
     lastListItem.focus();
-  }
+  };
 
   const handleClickShowDialog = () => {
     openDialog();
-  }
+  };
 
   const handleClickHideDialog = () => {
     closeDialog();
-  }
+  };
 
   const handleKeyDownShowDialog = (e: KeyboardEvent) => {
     if (e.code === "Enter") {
       openDialog();
       e.preventDefault();
     }
-  }
+  };
 
   const handleKeyDownHideDialog = (e: KeyboardEvent) => {
     if (e.code === "Enter" || e.code === "Escape") {
@@ -62,7 +62,7 @@ function HeaderBar() {
       focusOnLastListItem();
       e.preventDefault();
     }
-  }
+  };
 
   const handleKeyDownListItem = (e: KeyboardEvent) => {
     if (e.code === "Escape") {
@@ -70,7 +70,7 @@ function HeaderBar() {
       e.preventDefault();
       focusOnDialogControl();
     }
-  }
+  };
 
   const handleKeyDownFirstListItem = (e: KeyboardEvent) => {
     if (e.code === "Tab" && e.shiftKey) {
@@ -79,7 +79,7 @@ function HeaderBar() {
     } else {
       handleKeyDownListItem(e);
     }
-  }
+  };
 
   const handleKeyDownLastListItem = (e: KeyboardEvent) => {
     if (e.code === "Tab" && !e.shiftKey) {
@@ -88,11 +88,11 @@ function HeaderBar() {
     } else {
       handleKeyDownListItem(e);
     }
-  }
+  };
 
   return (
     <>
-      <header 
+      <header
         className="bg-white absolute flex justify-between items-center w-full p-4 md:py-0 lg:px-28 lg:py-0 z-20"
         aria-modal="true"
         aria-labelledby="nav"
@@ -102,18 +102,42 @@ function HeaderBar() {
 
         <button
           ref={dialogControlRef}
-          className="md:hidden" 
+          className="md:hidden"
           data-testid="menu"
-          onClick={showDialog ? handleClickHideDialog: handleClickShowDialog}
-          onKeyDown={showDialog ? handleKeyDownHideDialog : handleKeyDownShowDialog}
+          onClick={showDialog ? handleClickHideDialog : handleClickShowDialog}
+          onKeyDown={
+            showDialog ? handleKeyDownHideDialog : handleKeyDownShowDialog
+          }
         >
           {showDialog ? (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
             </svg>
           )}
         </button>
@@ -121,11 +145,19 @@ function HeaderBar() {
         <nav className="hidden md:block">
           <ul className="h-16">
             {listItems.map((listItem) => (
-              <li key={listItem} className="bg-gradient-to-r from-lime-green to-bright-cyan text-grayish-blue hover:text-dark-blue text-sm inline-block h-full">
-                <a href="#" className="bg-white flex justify-center items-center px-4 h-full hover:h-[95%]">{ listItem }</a>
+              <li
+                key={listItem}
+                className="bg-gradient-to-r from-lime-green to-bright-cyan text-grayish-blue hover:text-dark-blue text-sm inline-block h-full"
+              >
+                <a
+                  href="#"
+                  className="bg-white flex justify-center items-center px-4 h-full hover:h-[95%]"
+                >
+                  {listItem}
+                </a>
               </li>
             ))}
-          </ul> 
+          </ul>
         </nav>
 
         <div className="hidden md:block">
@@ -133,26 +165,44 @@ function HeaderBar() {
         </div>
       </header>
 
-      {showDialog && <div className="bg-gradient-to-b from-gray-800 to-transparent absolute flex justify-center items-center w-full top-0 bottom-0 z-10" role="dialog">
-        <nav className="text-dark-blue bg-white text-center w-10/12 py-4">
-          <ul>
-            {listItems.map((listItem) => (
-              <li key={listItem} className="py-2">
-                {listItem === "Home" ? (
-                  <a href="#" ref={firstListItemRef} onKeyDown={handleKeyDownFirstListItem}>{ listItem }</a>
-                ) : listItem === "Careers" ? (
-                  <a href="#" ref={lastListItemRef} onKeyDown={handleKeyDownLastListItem}>{ listItem }</a>
-                ) : (
-                  <a href="#" onKeyDown={handleKeyDownListItem}>{ listItem }</a>
-                )}
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>}
+      {showDialog && (
+        <div
+          className="bg-gradient-to-b from-gray-800 to-transparent absolute flex justify-center items-center w-full top-0 bottom-0 z-10"
+          role="dialog"
+        >
+          <nav className="text-dark-blue bg-white text-center w-10/12 py-4">
+            <ul>
+              {listItems.map((listItem) => (
+                <li key={listItem} className="py-2">
+                  {listItem === "Home" ? (
+                    <a
+                      href="#"
+                      ref={firstListItemRef}
+                      onKeyDown={handleKeyDownFirstListItem}
+                    >
+                      {listItem}
+                    </a>
+                  ) : listItem === "Careers" ? (
+                    <a
+                      href="#"
+                      ref={lastListItemRef}
+                      onKeyDown={handleKeyDownLastListItem}
+                    >
+                      {listItem}
+                    </a>
+                  ) : (
+                    <a href="#" onKeyDown={handleKeyDownListItem}>
+                      {listItem}
+                    </a>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      )}
     </>
-     
-  )
+  );
 }
 
 export default HeaderBar;
