71d540a5aab3f4848d030ff6fa46f1ce429d36f9
marouane <marsou431@gmail.com>
2022-11-23 14:20:27 +0100

    fix: change color on hover on anchor

--- components/AppFooterInfo/AppFooterInfo.tsx
+++ components/AppFooterInfo/AppFooterInfo.tsx
@@ -34,8 +34,8 @@ function AppFooterInfo() {
       <div className="second-group nav-links mb-6 md:translate-y-2">
         <ul className="text-sm list-none m-0 xs:columns-2 xs:max-w-[50%] xs:mx-auto md:max-w-none md:gap-4 lg:gap-16">
           {links.map((link) => (
-            <li key={link} className="text-very-light-gray hover:text-lime-green text-center pb-4 md:text-left">
-              <a href="#">{ link }</a>
+            <li key={link} className="text-center pb-4 md:text-left">
+              <a href="#" className="text-very-light-gray hover:text-lime-green">{ link }</a>
             </li>
           ))}
         </ul>
