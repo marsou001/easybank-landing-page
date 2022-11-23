0d68f92bab93e9755eb00addaf5192f102417d42
marouane <marsou431@gmail.com>
2022-11-23 14:35:03 +0100

    feat: add mockup image to Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,13 +1,15 @@
 import illustrationMobile from '../../public/images/bg-intro-mobile.svg';
 import illustrationDesktop from '../../public/images/bg-intro-desktop.svg';
+import mockups from '../../public/images/image-mockups.png';
 import RequestInviteButton from '../../shared/ui/RequestInviteButton';
 
 function Hero() {
   return (
-    <section className="bg-very-light-gray flex flex-col pb-16 md:flex-row-reverse md:justify-between md:items-center md:pb-0">
+    <section className="bg-very-light-gray relative flex flex-col pb-16 md:flex-row-reverse md:justify-between md:items-center md:pb-0">
       <div className="md:max-w-[55%] md:scale-125 md:translate-x-20">
         <img src={illustrationMobile} alt="illustration" className="md:hidden w-full h-full" />
         <img src={illustrationDesktop} alt="illustration" className="hidden md:inline-block" />
+        <img src={mockups.src} alt="mockups" className="absolute top-0 right-0 scale-75 -translate-y-20" />
       </div>
 
       <div className="text-center md:text-left px-4 md:max-w-[45%] lg:pl-28">
