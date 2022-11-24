8c364ae49355d87f059a4277598fb887d9abf488
marouane <marsou431@gmail.com>
2022-11-24 11:44:58 +0100

    feat: add title to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,14 @@ import Features from "../components/Features/Features";
 import AppBar from "../components/AppBar/AppBar";
 import Hero from "../components/Hero/Hero";
 import LatestArticles from "../components/LatestArticles/LatestArticles";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Easybank</title>
+      </Head>
       <AppBar />
       <main>
         <Hero />
